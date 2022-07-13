4b698c0c4dee009ce75e16b1615d3fa885be9406
Atanas Pavlov <nasko.js@gmail.com>
2022-07-13 18:46:34 +0300

    Added more fields to the ReactionKinetics example

--- reform_shared/src/main/scala/sysmo/reform/shared/form/examples/ReactionKinetics.scala
+++ reform_shared/src/main/scala/sysmo/reform/shared/form/examples/ReactionKinetics.scala
@@ -65,15 +65,55 @@ object ReactionKinetics extends FormModelHelper {
       val Solver = SolverBuilder
         .field(_("t_sim").descr("Simulation time"), _.float)
         .field(_("t_print").descr("Print interval"), _.float)
+        .field(_("abs_tol").descr("Absolute tolerance"), _.float)
+        .field(_("rel_tol").descr("Relative tolerance"), _.float)
         .build
 
+      val TimePlotLineBuilder =  field_group("TimePlotLine")
+
+      val TimePlotLine = TimePlotLineBuilder
+        .ref(_("species").descr("Species"), sp_ref)
+        .field(_("color").descr("Color"), _.select)
+        .field(_("marker").descr("Marker"), _.select)
+        .field(_("linestyle").descr("Line Style"), _.select)
+        .field(_("width").descr("Width"), _.float)
+        .field(_("legend_name").descr("Name in legend"), _.char)
+        .build
+
+      val AxisSettingsBuilder = field_group("AxisSettings")
+      val AxisSettings = AxisSettingsBuilder
+        .field(_("use_min").descr("Use min"), _.bool)
+        .field(_("min").descr("Min"), _.float)
+        .field(_("use_max").descr("Use max"), _.bool)
+        .field(_("max").descr("Max"), _.float)
+        .field(_("label").descr("Label"), _.char)
+        .field(_("label_font_size").descr("Label font size"), _.float)
+        .build
+
+      val PointSettingsBuilder = field_group("PointSettings")
+      val PointSettings = PointSettingsBuilder
+        .field(_("label").descr("Label"), _.char)
+        .field(_("color").descr("Color"), _.select)
+        .field(_("marker").descr("Marker"), _.select)
+        .build
+
+
       val TimePlot = TimePlotBuilder
-        .ref(_("species").descr("Species"), x => sp_ref(x).multiple())
+        .field(_("title").descr("Title"), _.char)
+        .field(_("title_font_size").descr("Font size"), _.float)
+        //.field(_("title_position").descr("Title position"), _.char)
+        .field(_("legend_position").descr("Legend position"), _.select)
+        .field(_("legend_font_size").descr("Legend font size"), _.float)
+        .group(_("x_settings").descr("X Range"), AxisSettingsBuilder)
+        .group(_("y_settings").descr("Y Range"), AxisSettingsBuilder)
+        .array(_("trajectories").descr("Trajectories"), TimePlotLineBuilder)
         .build
 
       val PhasePlot = PhasePlotBuilder
         .ref(_("x_var").descr("X variable"), sp_ref)
         .ref(_("y_var").descr("Y variable"), sp_ref)
+        .group(_("p_start").descr("Initial point"), PointSettingsBuilder)
+        .group(_("p_end").descr("Final point"), PointSettingsBuilder)
         .build
 
       val Simulation = SimulationBuilder
@@ -90,6 +130,9 @@ object ReactionKinetics extends FormModelHelper {
             Species("name" -> "Substrate", "symbol" -> "S"),
             Species("name" -> "Enzyme", "symbol" -> "E"),
             Species("name" -> "Product", "symbol" -> "P"),
+          ),
+          "plots" -> Seq(
+            TimePlot()
           )
         )
       )
